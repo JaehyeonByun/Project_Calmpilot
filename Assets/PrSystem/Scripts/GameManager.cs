741e16d9435d3a35e0468c15de9558b0ec71ff8a
JaHyun123 <bjh1750@gmail.com>
2023-11-21 01:59:13 +0900

    오류수정

--- Assets/PrSystem/Scripts/GameManager.cs
+++ Assets/PrSystem/Scripts/GameManager.cs
@@ -1,18 +1,73 @@
-using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.SocialPlatforms.Impl;
+using UnityEngine.UI;
+using static System.Net.Mime.MediaTypeNames;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    public enum GameState
     {
-        
+        MainMenu,
+        GameStart1,
+        GamePaused,
+        GameEnd
     }
 
+    public GameState gameState;
+
+    public void GameStart1()
+    {
+        SceneManager.LoadScene("Conference1");
+        gameState = GameState.GameStart1;
+    }
+    public void GameMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        gameState = GameState.MainMenu;
+    }
+    public void GameExit()
+    {
+        UnityEngine.Application.Quit();
+        gameState = GameState.GameEnd;
+    }
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    void Start()
+    {
+
+    }
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameState == GameState.MainMenu)
+        {
+
+        }
+        else if (gameState == GameState.GameStart1)
+        {
+
+        }
+        else if (gameState == GameState.GameEnd)
+        {
+
+        }
     }
 }
