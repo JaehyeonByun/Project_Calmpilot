4d2f055d03d006d0ff516110ed4f919f40da2331
류지혜/Jihye Ryu <51522587+Domwis-IR@users.noreply.github.com>
2023-12-13 17:23:56 +0900

    HyperRate + BHaptics Link
    Link the heartbeat rate with bHaptics Feedback

--- Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
+++ Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
@@ -10,6 +10,7 @@ using NativeWebSocket;
 using System.IO;
 using System.Text;
 
+
 public class NumberInfo
 {
     public DateTime TimeStamp { get; set; }
@@ -17,147 +18,169 @@ public class NumberInfo
     
 }
 
-public class hyperateSocket : MonoBehaviour
+namespace Bhaptics.SDK2
 {
-	// Put your websocket Token ID here
-    public string websocketToken2 = "iXiyxevGDyhWff3YnvEfpcCdKMJvArtSYyUkrXF6Siq1qhBrSkB9zsYVjGWhFPh3"; //You don't have one, get it here https://www.hyperate.io/api
-    public string hyperateID2 = "C21C";
-	// Textbox to display your heart rate in
-
-    List<NumberInfo> numberInfoList = new List<NumberInfo>();
-    Text textBox;
-	// Websocket for connection with Hyperate
-    WebSocket websocket;
-    async void Start()
+    public class hyperateSocket : MonoBehaviour
     {
-        textBox = GetComponent<Text>();
+        // Put your websocket Token ID here
+        public string websocketToken2 = "iXiyxevGDyhWff3YnvEfpcCdKMJvArtSYyUkrXF6Siq1qhBrSkB9zsYVjGWhFPh3"; //You don't have one, get it here https://www.hyperate.io/api
+        public string hyperateID2 = "C21C";
 
-        websocket = new WebSocket("wss://app.hyperate.io/socket/websocket?token=" + websocketToken2);
-        Debug.Log("Connect!");
+        // Textbox to display your heart rate in
+        List<NumberInfo> numberInfoList = new List<NumberInfo>();
+        Text textBox;
+        // Websocket for connection with Hyperate
+        WebSocket websocket;
 
-        websocket.OnOpen += () =>
+        // Variables for BHaptics Feedback
+        private float heartRateCheckInterval = 5f; // interval to check heart rate
+        private bool isHeartRateHigh = false; // flag to track if heart rate is high
+        private float timeSinceHighHeartRate = 0f; // time since heart rate first went high
+
+
+        async void Start()
         {
-            Debug.Log("Connection open!");
-            SendWebSocketMessage();
-        };
+            textBox = GetComponent<Text>();
 
-        websocket.OnError += (e) =>
-        {
-            Debug.Log("Error! " + e);
-        };
+            websocket = new WebSocket("wss://app.hyperate.io/socket/websocket?token=" + websocketToken2);
+            Debug.Log("Connect!");
 
-        websocket.OnClose += (e) =>
-        {
-            Debug.Log("Connection closed!");
-        };
-
-        string filePath = "example.csv"; // CSV 파일 경로 및 이름
-        
-        websocket.OnMessage += (bytes) =>
-        {
-        // getting the message as a string
-            var message = System.Text.Encoding.UTF8.GetString(bytes);
-            var msg = JObject.Parse(message);
-
-            string[] row1 = { "Charlie", (string)msg["payload"]["hr"], "매니저" };
-
-            NumberInfo info = new NumberInfo
+            websocket.OnOpen += () =>
             {
-                Value = (int)msg["payload"]["hr"], // 임의의 숫자 값 생성 (1부터 100 사이)
-                TimeStamp = DateTime.Now // 현재 시간 정보 저장
+                Debug.Log("Connection open!");
+                SendWebSocketMessage();
             };
 
-            numberInfoList.Add(info);
-
-            Debug.Log(numberInfoList[0].Value);
-
-            // CSV 파일 생성 및 데이터 쓰기
-            using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+            websocket.OnError += (e) =>
             {
-                sw.WriteLine(string.Join(",", row1));
-            }
+                Debug.Log("Error! " + e);
+            };
 
-            if (msg["event"].ToString() == "hr_update")
+            websocket.OnClose += (e) =>
             {
-                // Change textbox text into the newly received Heart Rate (integer like "86" which represents beats per minute)
-                textBox.text = (string) msg["payload"]["hr"];
-            }
-        };
+                Debug.Log("Connection closed!");
+            };
 
-        // Send heartbeat message every 25seconds in order to not suspended the connection
-        InvokeRepeating("SendHeartbeat", 1.0f, 25.0f);
+            string filePath = "example.csv"; // CSV 파일 경로 및 이름
 
-        // waiting for messages
-        await websocket.Connect();
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.T))
+            websocket.OnMessage += (bytes) =>
             {
-               SaveToCSV(); // CSV 파일로 저장하는 함수 호출
-            }
-    #if !UNITY_WEBGL || UNITY_EDITOR
-            websocket.DispatchMessageQueue();
-    #endif
-    }
+                // getting the message as a string
+                var message = System.Text.Encoding.UTF8.GetString(bytes);
+                var msg = JObject.Parse(message);
 
-    async void SendWebSocketMessage()
-    {
-        if (websocket.State == WebSocketState.Open)
-        {
-            // Log into the "internal-testing" channel
-            await websocket.SendText("{\"topic\": \"hr:"+hyperateID2+"\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 0}");
-        }
-    }
-    async void SendHeartbeat()
-    {
-        if (websocket.State == WebSocketState.Open)
-        {
-            // Send heartbeat message in order to not be suspended from the connection
-            await websocket.SendText("{\"topic\": \"phoenix\",\"event\": \"heartbeat\",\"payload\": {},\"ref\": 0}");
+                string[] row1 = { "Charlie", (string)msg["payload"]["hr"], "매니저" };
 
-        }
-    }
-
-    async void SaveToCSV() {
-        if (numberInfoList.Count == 0)
-        {
-            Console.WriteLine("저장된 데이터가 없습니다.");
-            return;
-        }
-
-        string csvFilePath = "NumberInfo.csv";
-
-        try
-        {
-            using (StreamWriter sw = new StreamWriter(csvFilePath, false, Encoding.UTF8))
-            {
-                // CSV 파일 헤더 작성
-                sw.WriteLine("TimeStamp,Value");
-
-                // 데이터 쓰기
-                foreach (var info in numberInfoList)
+                NumberInfo info = new NumberInfo
                 {
-                    string line = $"{info.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")},{info.Value}";
-                    sw.WriteLine(line);
+                    Value = (int)msg["payload"]["hr"], // 임의의 숫자 값 생성 (1부터 100 사이)
+                    TimeStamp = DateTime.Now // 현재 시간 정보 저장
+                };
+
+                numberInfoList.Add(info);
+
+                
+
+                // CSV 파일 생성 및 데이터 쓰기
+                using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+                {
+                    sw.WriteLine(string.Join(",", row1));
                 }
+
+                if (msg["event"].ToString() == "hr_update")
+                {
+                    // Change textbox text into the newly received Heart Rate (integer like "86" which represents beats per minute)
+                    textBox.text = (string)msg["payload"]["hr"];
+                }
+                if (info.Value >= 60)
+                {
+                    Debug.Log(info.Value);
+                    BhapticsLibrary.Play("eatfood"); // second haptic feedback
+                    isHeartRateHigh = false; // reset the state
+                }
+            };
+
+            // Send heartbeat message every 25seconds in order to not suspended the connection
+            InvokeRepeating("SendHeartbeat", 1.0f, 25.0f);
+
+            // Play CheckHeartRate
+            // InvokeRepeating("CheckHeartRate", 0f, heartRateCheckInterval);
+
+
+            // waiting for messages
+            await websocket.Connect();
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                SaveToCSV(); // CSV 파일로 저장하는 함수 호출
+            }
+#if !UNITY_WEBGL || UNITY_EDITOR
+            websocket.DispatchMessageQueue();
+#endif
+        }
+
+        async void SendWebSocketMessage()
+        {
+            if (websocket.State == WebSocketState.Open)
+            {
+                // Log into the "internal-testing" channel
+                await websocket.SendText("{\"topic\": \"hr:" + hyperateID2 + "\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 0}");
+            }
+        }
+        async void SendHeartbeat()
+        {
+            if (websocket.State == WebSocketState.Open)
+            {
+                // Send heartbeat message in order to not be suspended from the connection
+                await websocket.SendText("{\"topic\": \"phoenix\",\"event\": \"heartbeat\",\"payload\": {},\"ref\": 0}");
+
+            }
+        }
+
+        async void SaveToCSV()
+        {
+            if (numberInfoList.Count == 0)
+            {
+                Console.WriteLine("저장된 데이터가 없습니다.");
+                return;
             }
 
-            Console.WriteLine($"CSV 파일이 성공적으로 생성되었습니다. 경로: {Path.GetFullPath(csvFilePath)}");
+            string csvFilePath = "NumberInfo.csv";
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(csvFilePath, false, Encoding.UTF8))
+                {
+                    // CSV 파일 헤더 작성
+                    sw.WriteLine("TimeStamp,Value");
+
+                    // 데이터 쓰기
+                    foreach (var info in numberInfoList)
+                    {
+                        string line = $"{info.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")},{info.Value}";
+                        sw.WriteLine(line);
+                    }
+                }
+
+                Console.WriteLine($"CSV 파일이 성공적으로 생성되었습니다. 경로: {Path.GetFullPath(csvFilePath)}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"CSV 파일 생성 중 오류 발생: {ex.Message}");
+            }
         }
-        catch (Exception ex)
+
+        private async void OnApplicationQuit()
         {
-            Console.WriteLine($"CSV 파일 생성 중 오류 발생: {ex.Message}");
+            await websocket.Close();
         }
-    }
 
-    private async void OnApplicationQuit()
-    {
-        await websocket.Close();
     }
-
 }
+
 
 public class HyperateResponse
 {
