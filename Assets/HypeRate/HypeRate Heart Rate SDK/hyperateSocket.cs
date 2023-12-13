3abf4a332093d17feb9236d077bfcbc7fb896272
shgus1224 <shgus1224@naver.com>
2023-12-13 15:57:32 +0900

    feat: 심장박동에 따른 실시간 데이터 저장 및 csv 파일 저장기능 추가(t 클릭)

--- Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
+++ Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
@@ -7,12 +7,24 @@ using UnityEngine.UI;
 using Newtonsoft.Json.Linq;
 using NativeWebSocket;
 
+using System.IO;
+using System.Text;
+
+public class NumberInfo
+{
+    public DateTime TimeStamp { get; set; }
+    public int Value { get; set; }
+    
+}
+
 public class hyperateSocket : MonoBehaviour
 {
 	// Put your websocket Token ID here
-    public string websocketToken = "iXiyxevGDyhWff3YnvEfpcCdKMJvArtSYyUkrXF6Siq1qhBrSkB9zsYVjGWhFPh3"; //You don't have one, get it here https://www.hyperate.io/api
-    public string hyperateID = "internal-testing";
+    public string websocketToken2 = "iXiyxevGDyhWff3YnvEfpcCdKMJvArtSYyUkrXF6Siq1qhBrSkB9zsYVjGWhFPh3"; //You don't have one, get it here https://www.hyperate.io/api
+    public string hyperateID2 = "C21C";
 	// Textbox to display your heart rate in
+
+    List<NumberInfo> numberInfoList = new List<NumberInfo>();
     Text textBox;
 	// Websocket for connection with Hyperate
     WebSocket websocket;
@@ -20,7 +32,7 @@ public class hyperateSocket : MonoBehaviour
     {
         textBox = GetComponent<Text>();
 
-        websocket = new WebSocket("wss://app.hyperate.io/socket/websocket?token=" + websocketToken);
+        websocket = new WebSocket("wss://app.hyperate.io/socket/websocket?token=" + websocketToken2);
         Debug.Log("Connect!");
 
         websocket.OnOpen += () =>
@@ -39,11 +51,31 @@ public class hyperateSocket : MonoBehaviour
             Debug.Log("Connection closed!");
         };
 
+        string filePath = "example.csv"; // CSV 파일 경로 및 이름
+        
         websocket.OnMessage += (bytes) =>
         {
         // getting the message as a string
             var message = System.Text.Encoding.UTF8.GetString(bytes);
             var msg = JObject.Parse(message);
+
+            string[] row1 = { "Charlie", (string)msg["payload"]["hr"], "매니저" };
+
+            NumberInfo info = new NumberInfo
+            {
+                Value = (int)msg["payload"]["hr"], // 임의의 숫자 값 생성 (1부터 100 사이)
+                TimeStamp = DateTime.Now // 현재 시간 정보 저장
+            };
+
+            numberInfoList.Add(info);
+
+            Debug.Log(numberInfoList[0].Value);
+
+            // CSV 파일 생성 및 데이터 쓰기
+            using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
+            {
+                sw.WriteLine(string.Join(",", row1));
+            }
 
             if (msg["event"].ToString() == "hr_update")
             {
@@ -61,9 +93,13 @@ public class hyperateSocket : MonoBehaviour
 
     void Update()
     {
-#if !UNITY_WEBGL || UNITY_EDITOR
-        websocket.DispatchMessageQueue();
-#endif
+        if (Input.GetKeyDown(KeyCode.T))
+            {
+               SaveToCSV(); // CSV 파일로 저장하는 함수 호출
+            }
+    #if !UNITY_WEBGL || UNITY_EDITOR
+            websocket.DispatchMessageQueue();
+    #endif
     }
 
     async void SendWebSocketMessage()
@@ -71,7 +107,7 @@ public class hyperateSocket : MonoBehaviour
         if (websocket.State == WebSocketState.Open)
         {
             // Log into the "internal-testing" channel
-            await websocket.SendText("{\"topic\": \"hr:"+hyperateID+"\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 0}");
+            await websocket.SendText("{\"topic\": \"hr:"+hyperateID2+"\", \"event\": \"phx_join\", \"payload\": {}, \"ref\": 0}");
         }
     }
     async void SendHeartbeat()
@@ -81,6 +117,38 @@ public class hyperateSocket : MonoBehaviour
             // Send heartbeat message in order to not be suspended from the connection
             await websocket.SendText("{\"topic\": \"phoenix\",\"event\": \"heartbeat\",\"payload\": {},\"ref\": 0}");
 
+        }
+    }
+
+    async void SaveToCSV() {
+        if (numberInfoList.Count == 0)
+        {
+            Console.WriteLine("저장된 데이터가 없습니다.");
+            return;
+        }
+
+        string csvFilePath = "NumberInfo.csv";
+
+        try
+        {
+            using (StreamWriter sw = new StreamWriter(csvFilePath, false, Encoding.UTF8))
+            {
+                // CSV 파일 헤더 작성
+                sw.WriteLine("TimeStamp,Value");
+
+                // 데이터 쓰기
+                foreach (var info in numberInfoList)
+                {
+                    string line = $"{info.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")},{info.Value}";
+                    sw.WriteLine(line);
+                }
+            }
+
+            Console.WriteLine($"CSV 파일이 성공적으로 생성되었습니다. 경로: {Path.GetFullPath(csvFilePath)}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"CSV 파일 생성 중 오류 발생: {ex.Message}");
         }
     }
 
