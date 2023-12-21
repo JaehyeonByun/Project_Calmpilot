9ab58a72ff423038b157590c7fd3f9b0eb946815
JaHyun123 <bjh1750@gmail.com>
2023-12-21 21:47:44 +0900

    .

--- Assets/Scripts/Question.cs
+++ Assets/Scripts/Question.cs
@@ -1,18 +1,30 @@
+using Bhaptics.SDK2;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+
+
+
 
 public class Question : MonoBehaviour
 {
     public Animator QuestionAnimation;
     public AudioSource QuestionAudioSource;
     public GameObject Accept;
+    public hyperateSocket numberHapticList;
 
 
     public void OnButtonClick()
     {
         Invoke("DoQuestion", 2f);
+        /*ButtonInfo info = new ButtonInfo
+        {
+            TimeStamp = DateTime.Now, // 현재 시간 정보 저장
+            ButtonTriggered = true // Haptic feedback 여부
+        };
+        numberHapticList.Add(info);*/
     }
     public void DoQuestion()
     {
