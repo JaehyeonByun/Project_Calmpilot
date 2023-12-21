68bc9deab495cb9d8482a475a2169a69ef95e52c
Domwis-IR <domwis.IR@gmail.com>
2023-12-21 20:13:49 +0900

    Haptic feedback update by 5 seconds

--- Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
+++ Assets/HypeRate/HypeRate Heart Rate SDK/hyperateSocket.cs
@@ -15,7 +15,8 @@ public class NumberInfo
 {
     public DateTime TimeStamp { get; set; }
     public int Value { get; set; }
-    
+    public bool HapticFeedbackTriggered { get; set; }
+
 }
 
 namespace Bhaptics.SDK2
@@ -36,6 +37,7 @@ namespace Bhaptics.SDK2
         private float heartRateCheckInterval = 5f; // interval to check heart rate
         private bool isHeartRateHigh = false; // flag to track if heart rate is high
         private float timeSinceHighHeartRate = 0f; // time since heart rate first went high
+        private DateTime lastFeedbackTime = DateTime.MinValue;
 
 
         async void Start()
@@ -63,6 +65,7 @@ namespace Bhaptics.SDK2
 
             string filePath = "example.csv"; // CSV 파일 경로 및 이름
 
+
             websocket.OnMessage += (bytes) =>
             {
                 // getting the message as a string
@@ -70,16 +73,14 @@ namespace Bhaptics.SDK2
                 var msg = JObject.Parse(message);
 
                 string[] row1 = { "Charlie", (string)msg["payload"]["hr"], "매니저" };
+               
 
                 NumberInfo info = new NumberInfo
                 {
                     Value = (int)msg["payload"]["hr"], // 임의의 숫자 값 생성 (1부터 100 사이)
-                    TimeStamp = DateTime.Now // 현재 시간 정보 저장
+                    TimeStamp = DateTime.Now, // 현재 시간 정보 저장
+                    HapticFeedbackTriggered = false // Haptic feedback 여부
                 };
-
-                numberInfoList.Add(info);
-
-                
 
                 // CSV 파일 생성 및 데이터 쓰기
                 using (StreamWriter sw = new StreamWriter(filePath, false, Encoding.UTF8))
@@ -92,12 +93,19 @@ namespace Bhaptics.SDK2
                     // Change textbox text into the newly received Heart Rate (integer like "86" which represents beats per minute)
                     textBox.text = (string)msg["payload"]["hr"];
                 }
-                if (info.Value >= 60)
+                if ((DateTime.Now - lastFeedbackTime).TotalSeconds >= 4)
                 {
-                    Debug.Log(info.Value);
-                    BhapticsLibrary.Play("tap_tap_2"); // second haptic feedback
-                    isHeartRateHigh = false; // reset the state
+                    if (info.Value >= 60)
+                    {
+                        Debug.Log(info.Value);
+                        BhapticsLibrary.Play("tap_tap_2"); // second haptic feedback
+                        isHeartRateHigh = false; // reset the state
+                        lastFeedbackTime = DateTime.Now;
+                        info.HapticFeedbackTriggered = true;
+                    }
                 }
+
+                numberInfoList.Add(info);
             };
 
             // Send heartbeat message every 25seconds in order to not suspended the connection
@@ -155,12 +163,12 @@ namespace Bhaptics.SDK2
                 using (StreamWriter sw = new StreamWriter(csvFilePath, false, Encoding.UTF8))
                 {
                     // CSV 파일 헤더 작성
-                    sw.WriteLine("TimeStamp,Value");
+                    sw.WriteLine("TimeStamp,Value,HapticFeedback");
 
                     // 데이터 쓰기
                     foreach (var info in numberInfoList)
                     {
-                        string line = $"{info.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")},{info.Value}";
+                        string line = $"{info.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss")},{info.Value},{info.HapticFeedbackTriggered}";
                         sw.WriteLine(line);
                     }
                 }
